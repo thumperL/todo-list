a5eb68c04ce428f1935570615471e7a94cfe07cf
Thumper <shaunwhl1991@gmail.com>
2020-12-26 23:41:07 +1100

    update useCreateIndex db connection option due to deprecation
    update useCreateIndex db connection option due to deprecation

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost/todo-list', { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', () => {
   console.log('mongodb error!');
