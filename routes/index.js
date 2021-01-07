6c73c7939c1cca65bc9327db7aaef4867b395a6b
thumperL <shaunwhl1991@gmail.com>
2021-01-07 19:17:13 +1100

    feat: add user modules
    add user modules
    clean up router

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,15 @@ const express = require('express');
 
 const router = express.Router();
 
-// 準備引入路由模組
-// 引入 home 模組程式碼
+// Add routes modules
 const home = require('./modules/home');
-// 將網址結構符合 / 字串的 request 導向 home 模組
-router.use('/', home);
-
-// 引入 todos 模組程式碼
 const todos = require('./modules/todos');
-// 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組
+const users = require('./modules/users');
+
+// Route requests matching first var to the according modules
+router.use('/', home);
 router.use('/todos', todos);
+router.use('/users', users);
 
 // 匯出路由器
 module.exports = router;
