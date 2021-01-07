6df068d0c98eb868dabf11a23c5827e23f9dec09
thumperL <shaunwhl1991@gmail.com>
2021-01-07 19:16:45 +1100

    fix: mongoose url connection undefined error due to declaration
    fix: mongoose url connection undefined error due to declaration

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
-const { MONGODB_URI } = process.env || 'mongodb://localhost/todo-list';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/todo-list';
 
-mongoose.connect(MONGODB_URI, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+  MONGODB_URI,
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
 const db = mongoose.connection;
 db.on('error', () => {
   console.log('mongodb error!');
