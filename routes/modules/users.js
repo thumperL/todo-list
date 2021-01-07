c38eed2f59d72be34fbc28d918d24fd45085a337
thumperL <shaunwhl1991@gmail.com>
2021-01-07 19:20:06 +1100

    feat: add user login page
    add user login page
    add login post and register routes

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,16 @@
 const express = require('express');
 
 const router = express.Router();
+
+// Login
 router.get('/login', (req, res) => {
   res.render('login');
 });
+router.post('/login', (req, res) => {
+});
 
+// Register
+router.get('/register', (req, res) => {
+  res.render('register');
+});
 module.exports = router;
