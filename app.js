9874d85916b00dfef9ff4f7905f7f3e5ea0e9f32
Thumper <shaunwhl1991@gmail.com>
2021-01-12 00:08:04 +1100

    feat: add message partials
    feat: add message partials

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
 const usePassport = require('./config/passport');
 const Todo = require('./models/todo');
 
@@ -36,10 +37,13 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(methodOverride('_method'));
 usePassport(app);
+app.use(flash());
 app.use((req, res, next) => {
   // res.locals are accessible by views
   res.locals.isAuthenticated = req.isAuthenticated();
   res.locals.user = req.user;
+  res.locals.success_msg = req.flash('success_msg'); // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg'); // 設定 warning_msg 訊息
   next();
 });
 app.use(routes);
