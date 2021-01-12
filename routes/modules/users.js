37be3ceea86b6dfad51df14284fb0401400d4172
Thumper <shaunwhl1991@gmail.com>
2021-01-12 17:50:40 +1100

    feat: add bcrypt.js
    feat: add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const passport = require('passport');
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 
@@ -71,11 +72,14 @@ router.post('/register', (req, res) => {
         confirmPassword,
       });
     }
-    return User.create({
-      name,
-      email,
-      password,
-    })
+    return bcrypt
+      .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+      .then((salt) => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+      .then((hash) => User.create({
+        name,
+        email,
+        password: hash, // 用雜湊值取代原本的使用者密碼
+      }))
       .then(() => res.redirect('/'))
       .catch((err) => console.log(err));
   });
