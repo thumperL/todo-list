1c5d6f8d04974398be4b4127de5b7ce9c6104bc3
Thumper <shaunwhl1991@gmail.com>
2021-01-13 20:51:58 +1100

    modify todoSeeder
    modify todoSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,12 +1,35 @@
+const bcrypt = require('bcryptjs');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const Todo = require('../todo');
+const User = require('../user');
 const db = require('../../config/mongoose');
 
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678',
+};
 db.once('open', () => {
-  console.log('DB Connection SUCCESS');
-  for (let i = 0; i < 10; i++) {
-    Todo.create({
-      name: `name-${i}`,
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER.password, salt))
+    .then((hash) => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash,
+    }))
+    .then((user) => {
+      const userId = user._id;
+      return Promise.all(Array.from(
+        { length: 10 },
+        (value, i) => Todo.create({ name: `name-${i}`, userId }),
+      ));
+    })
+    .then(() => {
+      console.log('done.');
+      process.exit();
     });
-  }
-  console.log('Seeder Done');
 });
